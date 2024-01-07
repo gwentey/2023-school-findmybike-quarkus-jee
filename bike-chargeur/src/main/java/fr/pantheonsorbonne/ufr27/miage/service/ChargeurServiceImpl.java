b1f89a95a48523bca30f74a863b0e98ade7594ff
Anthony <anthonyoutub@gmail.com>
2024-01-07 10:29:24 +0100

    bike service improvement

--- bike-chargeur/src/main/java/fr/pantheonsorbonne/ufr27/miage/service/ChargeurServiceImpl.java
+++ bike-chargeur/src/main/java/fr/pantheonsorbonne/ufr27/miage/service/ChargeurServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @ApplicationScoped
 public class ChargeurServiceImpl implements ChargeurService {
-
+//test
 	private double chargeurPosX = 2.3522;
 	private double chargeurPosY = 48.8566;
 	@Inject
