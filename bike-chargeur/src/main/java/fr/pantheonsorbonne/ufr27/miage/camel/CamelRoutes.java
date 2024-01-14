2d9e87411defba79929e0ddcaaacecb32bf0bcf4
Anthony <anthonyoutub@gmail.com>
2024-01-14 19:33:54 +0100

    improve code

--- bike-chargeur/src/main/java/fr/pantheonsorbonne/ufr27/miage/camel/CamelRoutes.java
+++ bike-chargeur/src/main/java/fr/pantheonsorbonne/ufr27/miage/camel/CamelRoutes.java
@@ -21,27 +21,10 @@ public class CamelRoutes extends RouteBuilder {
     @Inject
     ChargeurService chargeurService;
 
-    @Inject
-    ChargeurGateway chargeurHandler;
-
     @Override
     public void configure() throws Exception {
 
         camelContext.setTracing(true);
-
-        /**
-         * Cette route consomme des messages de la queue 'M1.bike-recharge', qui contiennent des informations
-         * sur le/les vélos à recharger
-
-        from("sjms2:M1.bike-recharge")
-                .autoStartup(isRouteEnabled)
-                .unmarshal().json(Bike.class)
-                .process(exchange -> {
-                    Bike bike = exchange.getIn().getBody(Bike.class);
-
-                    chargeurHandler.chargerBike(bike);
-                });
-         */
 
 
         from("sjms2:M1.bike-recharge")
